408186e132b6fe3704e5e01efcc407ef2416cdbe
dgnsrekt <dgnsrekt@pm.me>
2020-06-01 01:50:06 -0500

    messing around.

--- lazy_ticker/ticker.py
+++ lazy_ticker/ticker.py
@@ -1,4 +1,5 @@
-from tda import auth, client
+from tda import auth
+from tda.client import Client
 from paths import PROJECT_ROOT, CHROMEDRIVER_LOCATION
 from decouple import config
 import json
@@ -8,21 +9,41 @@ TD_AMERITRADE_API_KEY = config("TD_AMERITRADE_API_KEY")
 TD_AMERITRADE_REDIRECT_URI = config("TD_AMERITRADE_REDIRECT_URI")
 
 try:
-    c = auth.client_from_token_file(TOKEN_PATH, TD_AMERITRADE_API_KEY)
+    api = auth.client_from_token_file(TOKEN_PATH, TD_AMERITRADE_API_KEY)
 except FileNotFoundError:
     from selenium import webdriver
 
     with webdriver.Chrome(CHROMEDRIVER_LOCATION) as driver:
-        c = auth.client_from_login_flow(
+        api = auth.client_from_login_flow(
             driver, TD_AMERITRADE_API_KEY, TD_AMERITRADE_REDIRECT_URI, TOKEN_PATH
         )
 
-r = c.get_price_history(
-    "AAPL",
-    period_type=client.Client.PriceHistory.PeriodType.YEAR,
-    period=client.Client.PriceHistory.Period.TWENTY_YEARS,
-    frequency_type=client.Client.PriceHistory.FrequencyType.DAILY,
-    frequency=client.Client.PriceHistory.Frequency.DAILY,
-)
-assert r.ok, r.raise_for_status()
-print(json.dumps(r.json(), indent=4))
+# NOTE: Think about a max chunking
+
+DATABASE = ["AAP"]
+search = ["AAPL", "TSLA", "GE", "SPY"]
+
+
+def filter_database(tickers):
+    if not tickers in DATABASE:
+        return True
+    else:
+        return False
+
+
+search = list(filter(filter_database, search))
+for s in search:
+    print(s)
+
+
+def verify_ticker(tickers):
+
+    response = api.search_instruments(
+        tickers, projection=Client.Instrument.Projection.SYMBOL_SEARCH,
+    )
+
+    assert response.ok, response.raise_for_status()
+    print(json.dumps(response.json(), indent=4))
+
+
+verify_ticker(search)
