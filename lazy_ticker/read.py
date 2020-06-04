9498a1f580fe4ecb5d26a13c86b9a29aa27a9391
dgnsrekt <dgnsrekt@pm.me>
2020-06-04 18:33:59 -0500

    Reading from the database.

--- lazy_ticker/read.py
+++ lazy_ticker/read.py
@@ -4,16 +4,23 @@ from time import sleep
 
 DATABASE_URI = config("DATABASE_URI")
 
+
 while True:
 
     table = pandas.read_sql_table("chartdata", DATABASE_URI)
 
-    columns = ["CHART_TIME", "OPEN_PRICE", "HIGH_PRICE", "LOW_PRICE", "CLOSE_PRICE"]
-    es = table[table["key"] == "/NQ"][columns]
+    columns = ["CHART_TIME", "OPEN_PRICE", "HIGH_PRICE", "LOW_PRICE", "CLOSE_PRICE", "VOLUME"]
+    es = table[table["key"] == "/GC"][columns]
 
     es = es.set_index("CHART_TIME")
     rsamp = es.resample("1H").agg(
-        {"OPEN_PRICE": "first", "HIGH_PRICE": "max", "LOW_PRICE": "min", "CLOSE_PRICE": "last"}
+        {
+            "OPEN_PRICE": "first",
+            "HIGH_PRICE": "max",
+            "LOW_PRICE": "min",
+            "CLOSE_PRICE": "last",
+            "VOLUME": "sum",
+        }
     )
     print(rsamp)
-    sleep(5)
+    sleep(30)
