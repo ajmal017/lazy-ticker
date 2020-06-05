71f896aa551c2ae80a17589336f04c56abc4f15e
dgnsrekt <dgnsrekt@pm.me>
2020-06-04 22:35:38 -0500

    added symbols command.

--- lazy_ticker/cli.py
+++ lazy_ticker/cli.py
@@ -1,4 +1,5 @@
 import typer
+from typing import List
 
 cli = typer.Typer()
 
@@ -6,6 +7,11 @@ cli = typer.Typer()
 @cli.command()
 def add_symbol(symbol: str):
     typer.echo(f"Adding {symbol} to database.")
+
+
+@cli.command()
+def add_symbols(symbols: List[str]):
+    typer.echo(f"Adding {symbols} to database.")
 
 
 if __name__ == "__main__":
