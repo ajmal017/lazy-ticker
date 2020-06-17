0e9d33c3ed35dd67f069775730b40bee13bbaf22
dgnsrekt <dgnsrekt@pm.me>
2020-06-16 19:36:11 -0500

    Added old twitter_scraper.

--- lazy_ticker/core.py
+++ lazy_ticker/core.py
@@ -1,5 +1,6 @@
 from lazy_ticker.database import LazyDB
 from lazy_ticker.schemas import InstrumentSchema
 from lazy_ticker.tda_scraper import *
+from lazy_ticker.twitter_scraper import *
 
 LazyDB.test_database_connection()
