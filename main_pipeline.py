8e3accb6d2c7f4b70711acb39588dd9e1ac7d445
dgnsrekt <dgnsrekt@pm.me>
2020-06-20 19:20:37 -0500

    Main pipeline pre watchlist method works needs heavy refactoring.

--- main_pipeline.py
+++ main_pipeline.py
@@ -8,12 +8,49 @@ from loguru import logger
 import pendulum
 import requests
 
+from lazy_ticker.tda_scraper import get_instruments
+from lazy_ticker.schemas import InstrumentsList
+from lazy_ticker.database import LazyDB
+from string import ascii_uppercase
+from random import choices
+from time import time
+
+import re
+
+
+def divide_chunks(container, size):
+    for position in range(0, len(container), size):
+        yield container[position : position + size]
+
+
+# TODO: heavy refactoring
+def prepare_for_watchlist():
+    logger.debug("prepare_for_watchlist starts")
+    valid_instruments = LazyDB.get_instruments()
+
+    if valid_instruments:
+        LazyDB.update_tweets(valid_instruments)
+
+    uncheck_symbols = LazyDB.get_uncheck_symbols_from_tweets()
+
+    if uncheck_symbols:
+        for chunk in divide_chunks(uncheck_symbols, 500):
+            valid = get_instruments(chunk).dict()["instruments"]
+            LazyDB.add_instruments(valid)
+
+        valid_instruments = LazyDB.get_instruments()
+        LazyDB.update_tweets(valid_instruments)
+
+    LazyDB.delete_invalid_tweets()
+
 
 def restore_usertable_from_previous_states():
+    # TODO: add config flag
     if DATA_DIRECTORY.exists():
         users = list(set(path.stem for path in DATA_DIRECTORY.glob("**/*.json")))
         for user in users:
             logger.debug(f"restoring {user} from previous state.")
+            # TODO you can animate this
             resp = requests.post(f"http://backend/user/{user}")
             assert resp.status_code == 201
 
@@ -25,37 +62,30 @@ def start_pipeline(timestamp):
     logger.debug("processing job", timestamp, dt, dt.timezone_name)
     logger.debug(f"data/{date}/users/{timestamp}.json")
 
-    # if get_all_users is empty
-    # if config says restart from old state
-    # than add users from old state
-
     # TODO: Add workers to config | TODO: dive into luigi config
     users = LazyDB.get_all_users()
 
     if not users:
         restore_usertable_from_previous_states()
 
-    luigi.build(
+    twitter_scrape_successful = luigi.build(
         [TwitterScraperPipline(timestamp=timestamp, users=users)], workers=3, local_scheduler=False
     )
-    # TODO: pipeline_task_successful =
-    # NOTE: look up external tasks
-    # validate symbols pipe
-    # get all distinct symbols / null valid
-    # get all instruments
-    # compare.
-    # any distinct symbols that are in instruments mark valid = True
 
-    # get all distinct symbols / null valid
-    # seach tda to look for valid symbols iter 500 at a time
-    # mark rows valid = True
-
-    # mark all other rows valid = False
-
-    # remove any invalid rows
+    if twitter_scrape_successful:
+        prepare_for_watchlist()  # better naming
+        logger.debug("prepare_for_watchlist complete!")
+    else:
+        logger.debug("notification of twitter scraping issue")
+        # TODO notification of scraping unsuccesful.
 
     # build watchlist one
     # build watchlist two
+
+    # TODO: clean_up_job(start) remove folders older than start task
+    # clean up task is optional. conig option
+    # only clean up if all the above tasks were successful and cleanup task setting is true
+    # clean up data folder date > 24 hours | max days back configuration | older than n days setting
 
 
 def task_loop(*, minute_interval: int, sleep_interval: int = 1):
@@ -83,10 +113,6 @@ def task_loop(*, minute_interval: int, sleep_interval: int = 1):
                     sleep(sleep_interval)
                 else:
                     start_pipeline(period.int_timestamp)
-                    # TODO: clean_up_job(start) remove folders older than start task
-                    # clean up task is optional. conig option
-                    # only clean up if all the above tasks were successful and cleanup task setting is true
-                    # clean up data folder date > 24 hours | max days back configuration | older than n days setting
 
 
 task_loop(minute_interval=1)
